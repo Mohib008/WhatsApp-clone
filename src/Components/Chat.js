0572961a5fb7e78d560ef1c8748d802c8f8336b9
Mohib Arsala <mohib.0008@gmail.com>
2020-10-09 07:12:56 -0400

    material ui add

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -4,7 +4,7 @@ import './Chat.css';
 function Chat() {
     return (
         <div className="chat">
-           <h1>i am from Chat HHHAha. </h1> 
+           
         </div>
     )
 }
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,19 +1,41 @@
 import React from 'react';
-//import ChatIcon from '@material-ui/icons/Chat';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import { Avatar, IconButton } from "@material-ui/core";
 import './Sidebar.css';
+import { SearchOutlined } from '@material-ui/icons';
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
     return (
         <div className="sidebar">
            <div className="sidebar__header">
+             <Avatar src="https://uploads.teamtreehouse.com/production/profile-photos/8288062/thumb_mohib1.jpg" alt="" />
                <div className="sidebar__headerRight">
                 <IconButton>
                    <DonutLargeIcon />
                 </IconButton>
-                 
+
+                <IconButton>
+                   <ChatIcon />
+                </IconButton>
+
+                <IconButton>
+                   <MoreVertIcon />
+                </IconButton> 
                </div>
+           </div>
+           <div className="sidebar__search">
+               <div className="sidebar__searchContainer">
+                   <SearchOutlined />
+                   <input type="text" placeholder="Search or Start new Chat" />
+               </div>
+           </div>
+           <div className="sidebar__chats">
+               <SidebarChat />
+               <SidebarChat />
+               <SidebarChat />
            </div>
         </div>
     )
