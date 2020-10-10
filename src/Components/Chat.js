84ebda65ae4eb0ba48af219070712e23876a8bfc
Mohib Arsala <mohib.0008@gmail.com>
2020-10-10 03:44:26 -0400

    chat section updated

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import SearchIcon from '@material-ui/icons/Search';
 import './Chat.css';
+//import { SearchOutlined } from '@material-ui/icons';
 
 function Chat() {
     return (
         <div className="chat">
-           
+          
+         <div className="chat__header">
+            <Avatar src="https://uploads.teamtreehouse.com/production/profile-photos/8288062/thumb_mohib1.jpg" alt="" />
+            <div className="chat__headerInfo">
+              <h3>Room Name</h3>
+              <p>Last seen at...</p>
+              </div>
+              <div className="chat__headerRight">
+                <IconButton>
+                 <SearchIcon />
+                </IconButton>
+          
+                <IconButton>
+                  <AttachFileIcon />
+                </IconButton>
+
+                <IconButton>
+                 <MoreVertIcon />
+                </IconButton>
+             </div>
+           </div>
         </div>
     )
 }
